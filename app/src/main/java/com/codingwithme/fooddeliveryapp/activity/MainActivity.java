b8ab24eea9ff732a863cd575794418698ec7b8dc
Атамас Мария <atamasmari@yandex.ru>
2023-09-23 15:47:03 +0300

    Initial commit

---
+++ app/src/main/java/com/codingwithme/fooddeliveryapp/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.codingwithme.fooddeliveryapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codingwithme.fooddeliveryapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
