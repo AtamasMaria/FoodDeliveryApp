04851718b9dcc9114a02e4aabe2637d273c41d28
Атамас Мария <atamasmari@yandex.ru>
2023-09-25 18:11:41 +0300

    1

--- app/src/main/java/com/codingwithme/fooddeliveryapp/activity/IntroActivity.java
+++ app/src/main/java/com/codingwithme/fooddeliveryapp/activity/IntroActivity.java
@@ -1,33 +1,27 @@
 package com.codingwithme.fooddeliveryapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.codingwithme.fooddeliveryapp.R;
 
-public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
-    private ConstraintLayout btnStart;
+public class IntroActivity extends AppCompatActivity {
+    private TextView startBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        Intent intent;
-
-        if (view.getId() == R.id.btnStart) {
-            intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
+        startBtn = findViewById(R.id.btnStart);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
     }
 }
